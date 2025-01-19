8d71d0d33f158bfd1909218b4c7b64be46564531
Fernando Herrera <herrera.fernando1209@gmail.com>
2025-01-18 21:55:08 -0300

    seccion 8 done!

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,13 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('main');
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port,
+        server: envs.natsServers,
       },
     },
   );
